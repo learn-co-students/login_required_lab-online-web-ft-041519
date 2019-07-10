3afcf5f9bf3b782289e6524d2576c95d433ce1c2
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-07-10 11:39:29 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
   def create           #login_path is same as (controller: 'sessions', action: 'new')
     return redirect_to login_path if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    redirect_to controller: 'application', action: 'hello'
+    redirect_to root_path # same as controller: 'application', action: 'hello'
   end
 
   def destroy
     session.delete :name
-    redirect_to controller: 'application', action: 'hello'
+    redirect_to root_path # same as controller: 'application', action: 'hello'
   end
 end
